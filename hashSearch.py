7a351f2658ac6f6a050042cdb5a5c6887d116348
alan <rightrice@proton.me>
2023-10-08 16:45:02 -0400

    Update hashSearch.py

--- hashSearch.py
+++ hashSearch.py
@@ -1,5 +1,8 @@
+import subprocess
+
 input_file_path = "trialsAndTribulations.txt"
 search_string = input("Enter the hash you want to search for: ")
+worldOfOysters = ["hashcat", "arg1", "arg2", "arg3"]
 
 with open(input_file_path, "r") as input_file:
     lines = input_file.readlines()
@@ -12,4 +15,14 @@ with open(input_file_path, "r") as input_file:
             matching_found = True
 
 if not matching_found:
-    print("Sorry, we dont currently have that hash. We will work to add it.")
+    print("Sorry, we dont currently have that hash. We will begin work on that hash.")
+    try:
+        result = subprocess.run(worldOfOysters, stdout=subprocess.PIPE, stdder=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            print("The system is now beginning work on the Hash.")
+            print(f"Hash Value: {result.stdout}")
+        else:
+            print("Sorry, the system couldn't do work to this hash.")
+
+    except Exception as e:
+        print("An error occured: ", str(e))
